3622439323e8526de0cfbb77d0a9dcd1b84bff9c
sofya <sofyauptuns@gmail.com>
2019-05-20 21:29:42 +0300

    init

---
+++ sort.h
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#ifndef UDT_SORT_H
+#define UDT_SORT_H
+
+#include "udt.h"
+
+void udtMergeSort(Udt *udt);
+void udtPartition(Udt *udt, Udt *udt1, Udt *udt2);
+void udtMerge(Udt *udt, Udt *udt1, Udt *udt2);
+
+#endif
